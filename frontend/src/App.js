d930a4b4e88b0148beef06aa297ea31d6effb6f1
wraith-0 <19ucs254@lnmiit.ac.in>
2022-07-15 15:24:43 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import "../styles/App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
